821717256e9d99da7f505a05e1a03968c2392a9c
aaron <moralesaaron1234@gmail.com>
2026-04-17 14:18:32 -0600

    reescribi code

--- app/ui/invoices/fonts.ts
+++ app/ui/invoices/fonts.ts
@@ -1,7 +1,4 @@
 import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
+export const lusitana = Lusitana({weight: ['400', '700'],subsets: ['latin'],});
