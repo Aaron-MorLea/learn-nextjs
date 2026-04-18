848ad7cea453464fad5badb2e395f975ce9e5dbe
aaron <moralesaaron1234@gmail.com>
2026-04-17 19:27:18 -0600

    ok

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,9 @@
 
-
-export default function invoices() {
+// app/dashboard/invoices/page.tsx
+export default function Invoices() {
     return (
         <div>
-            <h1>invoices</h1>
+            <h1>Invoices</h1>
         </div>
     );
 }
